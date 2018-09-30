a3c8bd1989516f1671a8a9729f0c1ec32f469dc5
Jake Treacher <git@jaketreacher.com>
2018-09-30 11:38:13 +0800

    Add media support

--- web/urls.py
+++ web/urls.py
@@ -12,8 +12,6 @@ urlpatterns = [
     path('contact', views.contact, name='contact'),
 ]
 
-if not settings.DEBUG:
-    urlpatterns += static(
-        settings.STATIC_URL,
-        document_root=settings.STATIC_ROOT
-    )
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
