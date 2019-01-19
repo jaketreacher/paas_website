aaf9ba6a177226bdda5205892d56a3271986d5a9
Jake Treacher <git@jaketreacher.com>
2019-01-20 07:39:04 +0800

    Show thumbnail in admin list_view

--- web/admin.py
+++ web/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.db.models import Count
 from django.conf import settings
+from django.utils.html import mark_safe
 
 from .models import Event, EventDateTime
 
@@ -11,7 +12,16 @@ class EventDateTimeInline(admin.TabularInline):
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug', 'open_date', 'event_range']
+    def thumbnail(self, obj):
+        img = '<img src="{url}" style="width:75px">'
+        if obj.thumbnail_img:
+            img = img.format(url=obj.thumbnail_img.url)
+        else:
+            img = img.format(url='https://via.placeholder.com/225x400')
+
+        return mark_safe(img)
+
+    list_display = ['name', 'slug', 'open_date', 'event_range', 'thumbnail']
     list_filter = ['open_date']
     search_fields = ['name']
     ordering = ['open_date']
