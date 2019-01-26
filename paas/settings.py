c6e1a8a3db2f70f19926a57df3567a234275fa66
Jake Treacher <git@jaketreacher.com>
2019-01-26 09:55:04 +0800

    Add OpenGraph meta tags

--- paas/settings.py
+++ paas/settings.py
@@ -13,9 +13,12 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 from decouple import config, Csv
 
+# Sites framework is required to use the `preferences` plugin, hence
+# `SITE_ID` must be explicity set
+SITE_ID = 1
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
--- web/templatetags/web_extras.py
+++ web/templatetags/web_extras.py
@@ -1,6 +1,7 @@
 from django import template
 from django.conf import settings
 from django.urls import reverse, NoReverseMatch
+from django.templatetags.static import StaticNode
 
 from os import path
 
@@ -19,3 +20,11 @@ def selected(context, name):
 @register.simple_tag(takes_context=True)
 def media(context, path):
     return path.join(settings.MEDIA_URL, path)
+
+
+@register.simple_tag(takes_context=True)
+def absolute_uri(context, url, resource_type=None):
+    request = context['request']
+    if resource_type == 'static':
+        url = StaticNode.handle_simple(url)
+    return request.build_absolute_uri(url)
